7b3724ec47f5e491771da088aaa352b872f33cdc
Huong Ngo <huong@huongngo.com>
2013-06-06 15:19:11 -0400

    making a copy of templates so they can be customized

--- lika/settings.py
+++ lika/settings.py
@@ -102,6 +102,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_DIR + '/apps/store/templates/oscar',
     PROJECT_DIR + '/apps/store/templates',
     PROJECT_DIR + '/templates',
 )
