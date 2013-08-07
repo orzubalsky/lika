7e89f37bade212a357c03b9b1345ca26417ab85f
orzubalsky <juviley@gmail.com>
2013-08-07 11:13:04 -0400

    making filen field not required and adding a field for vimeo embed code

--- lika/apps/catalogue/models.py
+++ lika/apps/catalogue/models.py
@@ -3,7 +3,8 @@ from django.db.models import *
 
 
 class Product(AbstractProduct):
-    filename = FileField(upload_to='files')
+    filename = FileField(upload_to='files', null=True, blank=True)
+    vimeo 	 = TextField(verbose_name=u'vimeo embed code', blank=True, null=True)
 
 
 from oscar.apps.catalogue.models import *
