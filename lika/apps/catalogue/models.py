69c6f01cab346f0e7af55cebc58855eeaeb75538
orzubalsky <juviley@gmail.com>
2013-08-17 18:26:20 -0400

    added difficulty_level field on Product model

--- lika/apps/catalogue/models.py
+++ lika/apps/catalogue/models.py
@@ -3,8 +3,17 @@ from django.db.models import *
 
 
 class Product(AbstractProduct):
+
+    DIFFICULTY_CHOICES = (
+        ('beginner', 'beginner'),
+        ('medium', 'medium'),
+        ('advanced', 'advanced'),
+    )
+
+
     filename = FileField(upload_to='files', null=True, blank=True)
-    vimeo 	 = TextField(verbose_name=u'vimeo embed code', blank=True, null=True)
+    vimeo = TextField(verbose_name=u'vimeo embed code', blank=True, null=True)
+    difficulty_level = CharField(max_length=10, choices=DIFFICULTY_CHOICES, blank=True, null=True, default='beginner')
 
 
 from oscar.apps.catalogue.models import *
